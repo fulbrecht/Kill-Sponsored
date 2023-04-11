48f3eac35d4ad9ebb12b0ff16ad935c34f13db22
Daniel Fulbrecht <13141050+fulbrecht@users.noreply.github.com>
2023-04-10 21:34:28 -0400

    test commit

--- scripts/content.js
+++ scripts/content.js
@@ -16,14 +16,12 @@ function killPost(textNode) {
   //kill inline sponsored posts 
   if(depth === inlineDepth) {
     sponsoredNode = parentNodes(textNode, inlineParents)
-    //sponsoredNode.parentNode.removeChild(sponsoredNode)
     sponsoredNode.style.display = "none";
   }
   //kill sponsored header posts
   else if(depth === headerDepth){
     sponsoredNode = parentNodes(textNode, headerParents).lastChild.firstChild
-    //sponsoredNode.style.display = "none";
-    sponsoredNode.parentNode.removeChild(sponsoredNode)
+    sponsoredNode.style.display = "none";
   }
   textNode.nodeValue = "😎"
   killCount ++
